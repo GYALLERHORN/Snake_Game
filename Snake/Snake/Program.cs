767b6b3e57b62b0f9add67a29400e978ac864c82
GYALLERHORN <kjins1998@gmail.com>
2023-08-18 16:22:17 +0900

    벽 만들기

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SnakeGame
+{
+    static void Main()
+    {
+        DrawWall();
+        for (int i = 0; i < 10; i++)
+        {
+            Thread.Sleep(500);
+        }
+    }
+    class Snake
+    {
+
+    }
+
+
+    static void DrawWall()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            Console.SetCursorPosition(i, 0);
+            Console.Write("#");
+            Console.SetCursorPosition(i, 20);
+            Console.Write("#");
+        }
+        for (int i = 0; i < 21; i++)
+        {
+            Console.SetCursorPosition(0, i);
+            Console.Write("#");
+            Console.SetCursorPosition(50, i);
+            Console.Write("#");
+        }
+    }
+
+    class Food
+    {
+
+    }
+
+    
+}
