d2e93070f44ef364635cddf00edfbc0dc18facca
GYALLERHORN <kjins1998@gmail.com>
2023-08-20 15:41:19 +0900

    뱀 일시 중지

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,11 +8,17 @@ class SnakeGame
         DrawWall();
 
         // 뱀 생성명령
-        Point c = new Point(10, 10, "ㅁ"); // 뱀의 머리 위치와 뱀의 몸통 모양을 정합니다. 이 c는 Point 클래스(객체)의 변수입니다.
-        Snake snake = new Snake(c, 3, Direction.RIGHT);
+        Point p = new Point(10, 10, "*"); // 뱀의 머리 위치와 뱀의 몸통 모양을 정합니다. 이 c는 Point 클래스(객체)의 변수입니다.
+        Snake snake = new Snake(p, 7, Direction.RIGHT);
         snake.SnakeDraw();
 
+        FoodCreator foodCreator = new FoodCreator(50, 20, "!");
+        Point foodPosition = foodCreator.CreateFoodPosition();
+        foodPosition.Draw();
+
         // 음식 생성명령
+
+
         // 뱀 동작명령
         // 뱀 충돌 감지
         // 
@@ -41,6 +47,8 @@ class SnakeGame
             symbol = _symbol; // _symbol이라는 모양을 넣을겁니다.
         }
 
+        
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
@@ -54,21 +62,25 @@ class SnakeGame
         public List<Point> wholeBody; // 뱀의 몸통 전체를 리스트 컬랙션으로 선언합니다. 리스트의 구성원은 몸통 한칸씩입니다.
         public Direction direction; // 뱀의 진행 방향입니다.
 
-        public Snake( Point body, int bodyLength,  Direction _direction )
+        public Snake( Point body, int bodyLength,  Direction _direction ) // 뱀의 생김새
         {
             direction = _direction;
             wholeBody = new List<Point>();
             for (int i = 0; i < bodyLength; i++)
             {
-                Point point = new Point(body.x, body.y, "ㅁ");
+                Point point = new Point(body.x, body.y, "*");
                 wholeBody.Add(point);
-                body.x += 1;
+                body.x -= 1;
             }
+
         }
 
         public void SnakeDraw()
         {
-
+            foreach (Point bodyPoint in wholeBody)
+            {
+                bodyPoint.Draw();
+            }
         }
         // 뱀 동작
         // 뱀 충돌 판별&상황별 동작
@@ -76,9 +88,25 @@ class SnakeGame
 
 
 
-    class Food
+    class FoodCreator
     {
+        public int FoodPositionX {  get; set; }
+        public int FoodPositionY { get; set; }
+        public string FoodMark { get; set; }
+        public FoodCreator(int foodX, int foodY, string foodMark)
+        {
+            FoodPositionX = foodX;
+            FoodPositionY = foodY;
+            FoodMark = foodMark;
+        }
         // 음식 생성
+        public Point CreateFoodPosition()
+        {
+            Random random = new Random();
+            int x = random.Next(1, FoodPositionX);
+            int y = random.Next(1, FoodPositionY);
+            return new Point(x, y, FoodMark);
+        }
         // 음식 재생성
     }
 
